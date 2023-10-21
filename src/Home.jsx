a5aba19e6c985394d34ba8c50ab663290c16db5d
hossan23 <imamhossan101@gmail.com>
2023-10-21 21:29:47 +0600

    added dark mode

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,31 @@
 import { useLoaderData } from 'react-router-dom';
 import HomeCard from './HomeCard';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
  const allData = useLoaderData();
- const[]
+
+ const [theme, setTheme] = useState('light');
+
+ useEffect(() => {
+  if (theme === 'dark') {
+   document.documentElement.classList.add('dark');
+  } else {
+   document.documentElement.classList.remove('dark');
+  }
+ }, [theme]);
+
+ const click = () => {
+  setTheme(theme === 'dark' ? 'light' : 'dark');
+ };
 
  return (
-  <div>
+  <div className="bg-white dark:bg-black ">
+   <div className="text-center">
+    <button className="btn px-2 " onClick={click}>
+     mode
+    </button>
+   </div>
    <div className="hero">
     <img src="https://img.freepik.com/free-vector/car-rental-service-sale-banner-template_23-2150461697.jpg?w=1380&t=st=1697812138~exp=1697812738~hmac=b3dd184edf39033bdc50d1c01275b5d688c8be00010fecade6cc155e7c66aefe" className="w-full" />
    </div>
@@ -18,7 +37,7 @@ const Home = () => {
    </div>
    <div className="mt-2 md:mt-10">
     {/* extra section -1 */}
-    <h1 className="text-2xl text-center mb-4 font-semibold">Live free with </h1>
+    <h1 className="text-2xl text-center mb-4 font-semibold dark:text-white">Live free with </h1>
     <div className="flex flex-col w-full lg:flex-row ">
      <div className="grid flex-grow  card bg-base-300 rounded-box place-items-center shadow-xl">
       <img src="https://images.pexels.com/photos/733745/pexels-photo-733745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
@@ -31,7 +50,7 @@ const Home = () => {
    </div>
    <div className="mt-2 md:mt-10">
     {/* extra section -2 */}
-    <h1 className="text-2xl text-center mb-4 font-semibold">Gallery </h1>
+    <h1 className="text-2xl text-center mb-4 font-semibold dark:text-white">Gallery </h1>
     <div className="carousel w-full">
      <div id="slide1" className="carousel-item relative w-full">
       <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full" />
